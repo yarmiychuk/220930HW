7ee04bd2772ec7b76395cc37a7aa22a342cbc40f
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-10-02 01:07:50 +0300

    small edition

--- 05.py
+++ 05.py
@@ -2,8 +2,8 @@
 # Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 
 def getList(num):
-    list = [-1, 1, 0, 1, 1]
-    for i in range(2, num):
+    list = [1, 0, 1]
+    for i in range(1, num):
         list.append(list[len(list) - 1] + list[len(list) - 2])
         list.insert(0, list[1] - list[0])
     return list
